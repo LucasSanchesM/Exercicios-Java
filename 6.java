4ab3132ac969c6edfe51c78be605fe82f36aa6ff
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-13 15:04:13 -0300

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,49 @@
+//5. Faça um programa que solicite ao usuário a entrada de 10 números 
+//inteiros e imprima como resultado a soma de todos os pares.
+
+import java.util.Scanner;
+public class Numeros {
+	private Scanner s;
+	private int[] numeros;
+	private int soma;
+	
+	public Numeros(){
+			//inicializar atributos
+			numeros= new int[10];
+			soma= 0;
+		}
+		
+	public void entrada(){
+		//colega dos 10 numeros inseridos pelo usuario utilizando um loop
+		s = new Scanner(System.in);
+		System.out.println("\nInsira 10 numeros");
+		for(int i=0; i<10; i++){
+			numeros[i]=s.nextInt();			
+		}		
+	}
+	
+	public void calcularPares(){
+		for(int i=0;i<10; i++){	
+			//utilização de loop para verificar se cada numero no array eh par
+			if(numeros[i]%2==0){
+				//caso seja par, sera adicionado em soma
+				soma+=numeros[i];										
+			}																										
+		}			
+	}
+	
+	public void impressao(){
+		//impressao do valor em soma
+		System.out.printf("\nA soma dos Pares eh igual a: %d", soma);						
+	}			
+}
+
+public class Main {
+	public static void main(String[] args) {
+			Numeros novoNum = new Numeros();
+			novoNum.entrada();
+			novoNum.calcularPares();
+			novoNum.impressao();
+	}
+}
+
