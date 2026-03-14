b0e9b87891bdbe7ca03afa2cced9f4609a4dc01e
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 18:09:37 -0300

    Update and rename 14.java to Numero.java

--- Exercicio14/Numero.java
+++ Exercicio14/Numero.java
@@ -58,14 +58,3 @@ public class Numero {
     
     
 }
-
-
-public class Main {
-	public static void main(String[] args) {
-		Numero novoNumero= new Numero();
-        novoNumero.entrada();
-        novoNumero.calculo();
-        novoNumero.imprimir();
-	}
-}
-
