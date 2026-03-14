fd7d02fad13421037ba865397aba61c24c08bfc2
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 17:30:29 -0300

    Atualizar o Salario.java

--- 01.java
+++ 01.java
@@ -34,13 +34,3 @@ public class Salario {
     
 }
 
-public class Exercicio01 {
-
-    public static void main(String[] args) {
-        Salario novoSalario= new Salario();
-        novoSalario.entrada();
-        novoSalario.atualizar();
-        novoSalario.imprimir();
-    }
-   
-}
