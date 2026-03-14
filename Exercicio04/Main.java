a8c67629565c3194fd783446f4e996b291b8db6c
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 18:00:15 -0300

    Create Main.java

---
+++ Exercicio04/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Triangulo novoTriangulo = new Triangulo();
+        novoTriangulo.entrada();
+        novoTriangulo.calcularT();
+        novoTriangulo.imprimir();
+    }
+    
+}
+
