e6e6546611feaca1038af0b6332bb25ce95cc452
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 18:10:03 -0300

    Create Main.java

---
+++ Exercicio14/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		Numero novoNumero= new Numero();
+        novoNumero.entrada();
+        novoNumero.calculo();
+        novoNumero.imprimir();
+	}
+}
+
+
